013c53327249e9acc1bf1f04e45eb9d51bd047e2
dmitrij <strelnikovdmitrij@gmail.com>
2014-11-08 22:59:20 +0100

    js slider

--- jmd/places/forms.py
+++ jmd/places/forms.py
@@ -3,13 +3,21 @@ __author__ = 'dmitrij'
 
 from django import forms
 
+
 class LocationForm(forms.Form):
     lat = forms.FloatField(widget=forms.HiddenInput())
     lng = forms.FloatField(widget=forms.HiddenInput())
     radius = forms.IntegerField(initial=500)
 
+    def __init__(self, *args, **kwargs):
+        super(LocationForm, self).__init__(*args, **kwargs)
+        self.fields['radius'].widget.attrs = {'data-slider': True, 'data-slider-range': "100,1000",
+                                              'data-slider-step': 100}
+
 
 from .models import Comment
+
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
