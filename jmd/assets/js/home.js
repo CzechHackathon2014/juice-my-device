79412892f5542b6c78d3f6808f11a8248d1997ea
dmitrij <strelnikovdmitrij@gmail.com>
2014-11-08 15:47:02 +0100

    some js loading

---
+++ jmd/assets/js/home.js
@@ -0,0 +1,19 @@
+///**
+// * Created by dmitrij on 08/11/14.
+// */
+//$(document).ready(function () {
+////    alert("ready!");
+//    latitude = $('#id_lat').val();
+//    latitude = $('#id_lng').val();
+//    radius = $('#id_rad').val();
+//    $.get("/places/around/", { lat: latitude, lng: latitude, radius: radius})
+//        .done(function (data) {
+//            div = $('#id_places');
+//
+//            for (var place in data.places){
+//                div.append(place);
+//            }
+//
+//
+//        });
+//});
--- jmd/places/views.py
+++ jmd/places/views.py
@@ -5,54 +5,81 @@ import foursquare
 from .forms import LocationForm
 from .models import Place
 
+
 def home(request, tmpl='places/home.html'):
     data = {}
     place_list = Place.objects.all()
     data['place_list'] = place_list
     return render(request, tmpl, data)
 
+
 def around(request, tmpl='places/around.html'):
     data = {}
     loc_form = LocationForm(request.GET or None)
     # print loc_form.is_valid()
 
-    if loc_form.is_valid():
-        frm = loc_form.cleaned_data
+    place_list = []
 
-        lat = frm.get('lat')
-        lng = frm.get('lng')
-        rad = frm.get('radius')
+    # if loc_form.is_valid():
+    #     frm = loc_form.cleaned_data
 
-        client = foursquare.Foursquare(client_id=settings.YOUR_CLIENT_ID, client_secret=settings.YOUR_CLIENT_SECRET)
+    lat = request.GET.get('lat')
+    lng = request.GET.get('lng')
+    rad = request.GET.get('radius')
 
-        # list_venues = client.venues.search(params={'ll': '%s,%s' % (lat, lng), 'radius':1000})
-        list_venues = client.venues.search(params={'ll': '%s,%s' % (lat, lng),  'radius': rad, 'categoryId':'4d4b7105d754a06374d81259'})
-        # list_venues = client.venues.search(params={'ll': '%s,%s' % (lat, lng), 'categoryId':'4d4b7105d754a06374d81259'})
+    client = foursquare.Foursquare(client_id=settings.YOUR_CLIENT_ID, client_secret=settings.YOUR_CLIENT_SECRET)
 
-        place_list = []
-
-        for v in list_venues['venues']:
-            lct = v['location']
-            # print lct['lng']
-            try:
-                pl = Place.objects.get(venue_uid=v['id'])
-                place_list.append(pl)
-            except Place.DoesNotExist:
-                pl = Place.objects.create(venue_uid=v['id'], name=v['name'], lat=lct['lat'], lng=lct['lng'])
+    # list_venues = client.venues.search(params={'ll': '%s,%s' % (lat, lng), 'radius':1000})
+    list_venues = client.venues.search(
+        params={'ll': '%s,%s' % (lat, lng), 'radius': rad, 'categoryId': '4d4b7105d754a06374d81259'})
+    # list_venues = client.venues.search(params={'ll': '%s,%s' % (lat, lng), 'categoryId':'4d4b7105d754a06374d81259'})
 
 
-                # print (list_venues)
-                # for venue in list_venues:
-                # print venue.name
-                place_list.append(pl)
+
+    for v in list_venues['venues']:
+        lct = v['location']
+        # print lct['lng']
+        try:
+            pl = Place.objects.get(venue_uid=v['id'])
+            place_list.append(pl)
+        except Place.DoesNotExist:
+            pl = Place.objects.create(venue_uid=v['id'], name=v['name'], lat=lct['lat'], lng=lct['lng'])
 
 
-                # data['venue'] = list_venues
-        data['place_list'] = place_list
-        data['places_count'] = len(place_list)
+            # print (list_venues)
+            # for venue in list_venues:
+            # print venue.name
+            place_list.append(pl)
+
+
+            # data['venue'] = list_venues
+    data['place_list'] = place_list
+    data['places_count'] = len(place_list)
+
+    if request.is_ajax():
+        from django.http import HttpResponse
+        import json
+        # print place_list
+        place_list = list(Place.objects.values('venue_uid', 'name', 'uuid'))
+
+        # for p in place_list:
+
+
+        pl_data = {'count': len(place_list), 'places': place_list}
+        print pl_data
+        # return HttpResponse(place_list)
+        # print json.dumps(place_list)
+
+        return HttpResponse(json.dumps(pl_data), content_type="application/json")
+
+
+    print 'should be set'
     return render(request, tmpl, data)
 
+
 from django.shortcuts import get_object_or_404
+
+
 def detail(request, uid, tmpl='places/detail.html'):
     data = {}
 
